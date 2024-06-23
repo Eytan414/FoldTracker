7943506da65e833bef60769083d7c642e08e4607
eytan414 <eytanivri@gmail.com>
2024-06-23 21:13:09 +0300

    basic impl

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # foldtracker README
 
-This is the README for your extension "foldtracker". After writing up a brief description, we recommend including the following sections.
+# ##################This is work in progress##########
+
 
 ## Features
 
@@ -14,13 +15,9 @@ For example if there is an image subfolder under your extension project workspac
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+If you have any requirements or dependencies
 
 ## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
 
 This extension contributes the following settings:
 
@@ -29,29 +26,17 @@ This extension contributes the following settings:
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
 
-### 1.0.0
+### 0.0.2
 
-Initial release of ...
+Initial release of FoldTracker
 
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
 
 ## Following extension guidelines
 
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
 
 * [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
 
@@ -64,8 +49,3 @@ You can author your README using Visual Studio Code. Here are some useful editor
 * Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
 
 ## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,30 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('extension.showLineCountInFoldedSections', () => {
+        if (vscode.window.activeTextEditor) {
+            const editor = vscode.window.activeTextEditor;
+            const foldedRegions = editor.visibleRanges.filter(range => range.start.line !== range.end.line);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "foldtracker" is now active!');
+            foldedRegions.forEach(range => {
+                const lineCount = range.end.line - range.start.line;
+                const decoration = vscode.window.createTextEditorDecorationType({
+                    after: {
+                        contentText: ` (${lineCount} lines)`,
+                        color: 'gray',
+                        margin: '0px 0px 0px 2px'
+                    }
+                });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('foldtracker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from FoldTracker!');
-	});
+                editor.setDecorations(decoration, [range]);
+                context.subscriptions.push(decoration);
+            });
+        }
+    });
+
 
 	context.subscriptions.push(disposable);
-}
-
+  }
+  
 // This method is called when your extension is deactivated
 export function deactivate() {}
